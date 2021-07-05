3024505dd61d3d42946d1c87d2778c866be7fe57
Chathura <c.rathnayake97@gmail.com>
2021-07-06 03:46:51 +0530

    Integrated material-UI libraries

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import { Button } from "@material-ui/core";
 
 class Navbar extends Component {
   render() {
     return (
       <div>
-        <h1>the nav bar</h1>
+        <Button color="secondary">The nav bar </Button>
       </div>
     );
   }
