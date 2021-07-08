0076439986e6099bd0fc45d1beb425fc12ce6f0f
Iranga Mudalige <irangapriyam@gmail.com>
2021-07-08 21:41:46 +0530

    Login ui added

---
+++ src/views/login.jsx
@@ -0,0 +1,157 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import image from './soteriax.png';
+import Link from '@material-ui/core/Link';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+const useStyles = makeStyles((theme) => ({
+    bgImage:{
+        opacity: 0.4,
+        position: 'absolute',
+        left: 0,
+        top: 0,
+        width: "100%",
+        height: '100%',
+    },
+    mainDiv:{
+        position:"relative",
+        //opacity:"1 !important",
+        boarder:"20%",
+        backgroundColor: "white",
+        borderRadius: "10px",
+    },
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      },
+      avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+      },
+      form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+      },
+      submit: {
+        margin: theme.spacing(3, 0, 2),
+        color:"fa931d",
+      },
+      root: {
+        '& > *': {
+          margin: theme.spacing(1),
+        },
+        size:"50px",
+      },
+      IconButton:{
+          height:"30px",
+          width:"30px",
+      }
+
+}));
+
+
+
+const Login = () => {
+    const classes = useStyles();
+
+    return (
+    <div>
+          <img
+    className={classes.bgImage}
+    src="https://images.unsplash.com/photo-1611222566295-885a2c99153a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+    alt=""
+  ></img>
+    <div >
+        
+    <Container component="main" maxWidth="xs" className={classes.mainDiv}>
+      <CssBaseline />
+      <div className={classes.paper}>
+        {/* <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar> */}
+        <img src={image} height={100} width={100} />
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+
+        
+        {/* <div className={classes.root}>
+            <IconButton aria-label="delete">
+            <img src="https://img.icons8.com/office/16/000000/google-logo.png" className={classes.IconButton}/>
+            </IconButton>
+      
+        </div> */}
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            //style={{backgroundColor: '#fa931d', color: '#FFFFFF'}}
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+            <div style={{alignItems: 'center'}}>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </div>
+        </form> 
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+
+        </div></div>
+   
+    );
+}
+
+export default Login;
