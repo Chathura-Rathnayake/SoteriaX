447bd3dca6add687fdf459bb8808bed1d5846223
Chathura <c.rathnayake97@gmail.com>
2021-07-10 03:02:27 +0530

    Modified the nav bar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,6 +4,8 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
+import logo from "../assets/icons/logoWhiteWithoutName.png";
+import "../assets/css/navbar.css";
 
 const useStyles = makeStyles(() => ({
   palette: {
@@ -16,6 +18,10 @@ const useStyles = makeStyles(() => ({
   },
   title: {
     flexGrow: 1,
+    fontFamily: ['"Orbitron"', 'sans-serif'].join(','),
+    fontSize:25,
+    // font:,
+    // fontFamily:'Orbitron',sans-serif;
   },
   barWidth: {
     height: 70,
@@ -37,13 +43,20 @@ export default function NavBar() {
         color="primary"
       >
         <Toolbar>
+          <img src={logo} alt="" style={{ width: 100, height: 60 }} />
           <Typography variant="h6" className={classes.title}>
             SoteriaX
           </Typography>
-          <Button variant="text" style={{ marginRight: 50, fontWeight:"bold",color:"white" }}>
+          <Button
+            variant="text"
+            style={{ marginRight: 50, fontWeight: "bold", color: "white" }}
+          >
             Contact Us
           </Button>
-          <Button variant="text" style={{ marginRight: 50 ,fontWeight:"bold",color:"white" }}>
+          <Button
+            variant="text"
+            style={{ marginRight: 50, fontWeight: "bold", color: "white" }}
+          >
             About Us
           </Button>
           <Button color="primary" variant="contained" href="/login">
