a781a184bceaab0c7a406beca5d2c49dc123ad26
ShanukaNF <56572468+ShanukaNF@users.noreply.github.com>
2021-08-28 19:21:47 +0530

    trial and error backend

--- backend/server.js
+++ backend/server.js
@@ -83,5 +83,40 @@ app.post("/headguardSupport", function (req,res) {
 
 });
 
+app.get("/adminSuggestion", function (req, res) {
+  //doing a read
+  var docRef = db.collection("Help Requests");
+  docRef
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        console.log("Document data:", doc.data());
+        //sending the response to the frontend
+        res.json({
+          accountType: doc.data().accountType,
+          companyId: doc.data().companyId,
+          headline: doc.data().headline,
+          msg: doc.data().msg,
+          status: doc.data().status,
+          viewed: doc.data().viewed,
+        });
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    })
+    .catch((error) => {
+      console.log("Error getting document:", error);
+    });
+
+
+  //   const citiesRef = db.collection('cities');
+  //     const snapshot = await citiesRef.get();
+  //     snapshot.forEach(doc => {
+  //     console.log(doc.id, '=>', doc.data());
+  //     });
+
+});
+
 
 app.listen(process.env.PORT || 8080);
