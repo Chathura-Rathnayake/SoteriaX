5bcf97fc3e9855bb5a9765de4a9248271bc78456
Chathura <c.rathnayake97@gmail.com>
2021-08-30 14:56:11 +0530

    Performing backend calls via async/await

--- frontend/src/views/headAdmin/ADashboard.js
+++ frontend/src/views/headAdmin/ADashboard.js
@@ -16,19 +16,41 @@ const useStyles = makeStyles({
 
 export default function ADashboard() {
   const classes = useStyles();
+  const { currentUser } = useAuth();
+
+  // //performing an API request
+  // useAuth()
+  //   .currentUser.getIdToken(true) //getting the currently logged in user's id token from firebase
+  //   .then((idToken) => {
+
+  //     //the complete object that is needed to sent to the backend
+  //     const toSend = {
+  //       username: "harcana", //some other data
+  //       token: idToken, //the token
+  //     };
+
+  //     // Send the data to the backend
+  //     fetch("/send", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-type": "application/json",
+  //       },
+  //       body: JSON.stringify(toSend),
+  //     })
+  //       .then((res) => res.json()) //retrieving the request from backend
+  //       .then((data) => console.log(data)); //printing it to the console
+  //   })
+  //   .catch(function (error) {
+  //     // Handle the error
+  //   });
 
   //performing an API request
-  useAuth()
-    .currentUser.getIdToken(true) //getting the currently logged in user's id token from firebase
-    .then((idToken) => {
-
-      //the complete object that is needed to sent to the backend
-      const toSend = {
-        username: "harcana", //some other data
-        token: idToken, //the token
-      };
-
-      // Send the data to the backend
+  async function myFunc() {
+    const idToken = await currentUser.getIdToken(true); //get the token of the current user
+    const toSend = {
+      token: idToken,
+    };
+    try {
       fetch("/send", {
         method: "POST",
         headers: {
@@ -38,16 +60,42 @@ export default function ADashboard() {
       })
         .then((res) => res.json()) //retrieving the request from backend
         .then((data) => console.log(data)); //printing it to the console
-    })
-    .catch(function (error) {
-      // Handle the error
-    });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  myFunc(); //executing the async function
+
+  //an example on how to do this inside the useEffect hook
+  useEffect(() => {
+    async function someFunc() {
+      const idToken = await currentUser.getIdToken(true); //get the token of the current user
+      const toSend = {
+        token: idToken,
+      };
+      try {
+        fetch("/send", {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify(toSend),
+        })
+          .then((res) => res.json()) //retrieving the request from backend
+          .then((data) => console.log(data)); //printing it to the console
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    someFunc(); //executing it
+  }, []);
 
   return (
     <Layout>
       <Container size="sm">
         <Typography variant="h6" color="textSecondary">
-          Dashboard - Lifecorp(Pvt) Ltd
+          Dashboard - Lifecorp(Pvt) Ltd - {currentUser.email}
         </Typography>
 
         <Simplemap />
