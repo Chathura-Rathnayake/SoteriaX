e95e528d6aa3ad630c43606bf5005df2514f9466
nukezzzz <lawthenuka@gmail.com>
2021-09-01 00:16:33 +0530

    fixed geo map error

--- frontend/src/components/headAdmin/Simplemap.js
+++ frontend/src/components/headAdmin/Simplemap.js
@@ -1,9 +1,8 @@
-import "mapbox-gl/dist/mapbox-gl.css";
-import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
+
 import React, { useState, useRef, useCallback } from "react";
 import { render } from "react-dom";
 import MapGL from 'react-map-gl';
-import Geocoder from "react-map-gl-geocoder";
+
 
 
 export default function Simplemap() {
@@ -11,7 +10,7 @@ export default function Simplemap() {
         latitude: 8.064669,
         longitude: 439.448345,
         zoom: 6,
-        countries: 'LK'
+        
     })
     const MAPBOX_TOKEN ="pk.eyJ1IjoibnVrZXp6eiIsImEiOiJja3F0NWlqaDAwcGpzMnBucXk0anBpN3duIn0.tibmy5QPalU6q9_bvGkb6Q";
     const mapRef = useRef();
@@ -19,17 +18,7 @@ export default function Simplemap() {
       (newViewport) => setViewport(newViewport),
       []
     );
-    const handleGeocoderViewportChange = useCallback(
-        (newViewport) => {
-          const geocoderDefaultOverrides = { transitionDuration: 1000 };
-    
-          return handleViewportChange({
-            ...newViewport,
-            ...geocoderDefaultOverrides
-          });
-        },
-        [handleViewportChange]
-      );    
+
     return (
         <div style={{ height: "65vh" }}>
         <MapGL
@@ -41,14 +30,7 @@ export default function Simplemap() {
           mapboxApiAccessToken={MAPBOX_TOKEN}
           mapStyle="mapbox://styles/nukezzz/ckqy7z6nk09cp18s7nao9mmy9"
         >
-          <Geocoder
-            mapRef={mapRef}
-            countries='LK'
-            onViewportChange={handleGeocoderViewportChange}
-            mapboxApiAccessToken={MAPBOX_TOKEN}
-            position="top-left"
-            zoom= {14}wwwww
-          />
+
         </MapGL>
       </div>
     );
