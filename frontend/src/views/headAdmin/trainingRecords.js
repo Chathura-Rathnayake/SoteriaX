378d821f2018141a33c670a64fd5cf39251fb7d4
nukezzzz <lawthenuka@gmail.com>
2021-09-05 02:07:10 +0530

    Added Traing data view dialog and UI

--- frontend/src/views/headAdmin/trainingRecords.js
+++ frontend/src/views/headAdmin/trainingRecords.js
@@ -8,275 +8,252 @@ import Layout from "../../components/headAdmin/Layout";
 import { TextField } from "@material-ui/core";
 import Select from "@material-ui/core/Select";
 import { useAuth } from "../../contexts/AuthContext.js";
-import { useHistory } from "react-router-dom";
 import Notification from "../../components/headAdmin/Notification";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
-import IconButton from "@material-ui/core/IconButton";
-import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
-import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 import { DataGrid } from '@material-ui/data-grid';
+import AlertDialogSlide from "../../components/headAdmin/dialog";
+import AlertDialog from "../../components/headAdmin/TrainRecordDeleteConfirm";
 
-const useStyles = makeStyles({
-    // root: {
-    //     width: "100%",
-    //     overflowX: "auto",
-    // },
-    // table: {
-    //     minWidth: 650,
-    // },
-    bot: {
-        marginBottom: 10,
-    },
-    top50: {
-        marginTop: 20,
-    },
-    top70: {
-        marginTop: 70,
-    },
-    card: {
-        marginTop: "30px",
-        marginLeft: "50px",
-    },
-    bold400: {
-        fontWeight: 600,
-    },
-});
-
-// function createData(name, calories, fat, carbs, protein, num1, num2) {
-//   return { name, calories, fat, carbs, protein, num1, num2 };
-// }
-
-// const rows = [
-//   createData("Frozen yoghurt", 159, 6.0, 24, 23, [
-//     {
-//       id: "EMP-LG-032",
-//       username: "RonPerera",
-//       lastName: "Perera",
-//       firstName: "Ron",
-//       age: 35,
-//     },
-//   ]),
-// ];
-
-// const ExpandableTableRow = ({ children, expandComponent, ...otherProps }) => {
-//     const [isExpanded, setIsExpanded] = React.useState(false);
-
-//     return (
-//         <>
-//             <TableRow {...otherProps}>
-//                 <TableCell padding="checkbox">
-//                     <IconButton onClick={() => setIsExpanded(!isExpanded)}>
-//                         {isExpanded ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-//                     </IconButton>
-//                 </TableCell>
-//                 {children}
-//             </TableRow>
-//             {isExpanded && (
-//                 <TableRow>
-
-//                     {expandComponent}
-//                 </TableRow>
-//             )}
-//         </>
-//     );
-// };
 
 export default function TrainingRecords() {
-    const [notify, setNotify] = useState({
-        isOpen: false,
-        message: "",
-        type: "",
-        num: "",
-    });
-    const classes = useStyles();
-    const [data, getData] = useState([]);
-    const { currentUser } = useAuth();
-    const renderDetailsButton = (params) => {
-        return (
-            <strong>
-                <Button
-                    variant="contained"
-                    color="primary"
-                    size="small"
-                    style={{ marginLeft: 16 }}
-                    onClick={() => {
-                        
-                    }}
-                >
-                    view Details
-                </Button>
-            </strong>
-        )
-      }
-      const columns = [
-        {
-            field: 'id', 
-            headerName: 'Unique-ID', 
-            width: 220 , 
-            headerAlign: 'left',
-         },
-        {
-            field: "title", 
-            headerName: 'Session title', 
-            width: 180 , 
-            headerAlign: 'left',
-         },
+  const [Alldata, getAllData] = useState([]);
+  const [open, setOpen] = useState(false)
+  const [passingDataDelete, setPassingDataDelete] = useState([]);
+  const [openConfirm, setOpenConfirm] = useState(false)
+  const [passingData, setPassingData] = useState([]);
+  const [passingDataParticipants, setPassingDataParticipants] = useState([]);
+  const [passingDataTrainingTimes, setPassingTrainingTimes] = useState([]);
+  const { currentUser } = useAuth();
 
-        {
-          field: 'date',
-          headerName: 'Sheduled Date',
-          width: 170,
-          headerAlign: 'center',
-        },
-          {
-            field: 'startTime',
-            headerName: 'Sheduled Time',
-            width: 150,
-            headerAlign: 'center',
-          },
-        //   {
-        //     field: 'lastName',
-        //     headerName: 'Last name',
-        //     width: 150,
-        //     headerAlign: 'center',
-        //   },
+  const handleClick = (e, cellValues) => {
+    console.log('cell val', cellValues.row.trainingTimes);
+    setOpen(true)
+    setPassingData(cellValues.row)
+    setPassingDataParticipants(cellValues.row.participants)
+    setPassingTrainingTimes(cellValues.row.trainingTimes)
+  };
+  const handleClickDelete = (e, cellValues) => {
 
-          {
-            field: 'Edit',
-            headerName: 'Edit',
-            renderCell: renderDetailsButton,
-            disableClickEventBubbling: true,
-            width: 200,
-            align:'center',
-            headerAlign: 'center',
-            
-          },
-        ];
 
-    useEffect(() => {
-        async function getList() {
-            const idToken = await currentUser.getIdToken(true); //get the token of the current user
-            const toSend = {
-                token: idToken,
-            };
-            try {
-                fetch("/getTrainingRecords", {
-                    method: "POST",
-                    headers: {
-                        "Content-type": "application/json",
-                    },
-                    body: JSON.stringify(toSend),
-                })
-                    .then((res) => res.json()) //retrieving the request from backend
-                    .then((data) => getData(data)); //printing it to the console
-            } catch (err) { }
-        }
-        getList(); //executing it
-    }, []);
+    setOpenConfirm(true)
+    setPassingDataDelete(cellValues.row)
 
-    console.log(data);
+    //  getAllData(Alldata.filter((record) => record.id !== cellValues.row.id))
+    
+  };
+  const renderDetailsButton = (params) => {
     return (
-        <Layout>
-            <Container size="sm">
-                {/* <div class={classes.top50}>
-                    <Typography
-                        align="center"
-                        variant="h5"
-                        className={classes.bold400}
-                        color="textSecondary"
-                    >
-                        Training Records
-                    </Typography>
-                </div>
-                <div class={classes.top70}></div>
-                <Grid>
-                    <Grid item xs={12}>
-                        <Paper className={classes.root}>
-                            <Table className={classes.table} aria-label="simple table">
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell padding="checkbox" />
-                                        <TableCell>Session Title</TableCell>
-                                        <TableCell align="right">Scheduled Date</TableCell>
-                                        <TableCell align="right">Scheduled Time</TableCell>
-                                        <TableCell align="right">Time-Taken</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {data.map((row) => (
-                                        <ExpandableTableRow
-                                            key={row.docID}
-                                            expandComponent={
-                                                <Grid>
-                                                    <TableRow>
-                                                        <TableCell colSpan="2">Description : </TableCell>
-                                                        <TableCell colSpan="3">{row.summary}</TableCell>
-                                                    </TableRow>
-                                                </Grid>
+      <strong>
+        <Button
+          variant="contained"
+          color="primary"
+          size="small"
+          style={{ marginLeft: 16 }}
+          onClick={(e) => {
+            handleClick(e, params)
+          }}
+        >
+          View More
+        </Button>
+      </strong>
+    )
+  }
 
-                                            }
-                                        >
-                                            <TableCell component="th" scope="row">
-                                                {row.docID}
-                                            </TableCell>
-                                            <TableCell align="right">{row.date}</TableCell>
-                                            <TableCell align="right">{row.startTime}</TableCell>
-                                            <TableCell align="right">{row.trainingTimes[4]}</TableCell>
-                                        </ExpandableTableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
-                        </Paper>
+  const renderPendingDetailsEditButton = (params) => {
+    return (
+      <strong>
+        <Button
+          variant="contained"
+          color="primary"
+          size="small"
+          style={{ marginLeft: 16 }}
+          onClick={(e) => {
+            handleClick(e, params)
+          }}
+        >
+          View More
+        </Button>
+      </strong>
+    )
+  }
 
-                    </Grid>
 
-                    <div class={classes.top70}></div>
-                    <Grid>
+  const renderDeleteButton = (params) => {
+    return (
+      <strong>
+        <Button
+          variant="contained"
+          color="secondary"
+          size="small"
+          style={{ marginLeft: 16 }}
+          onClick={(e) => {
+            handleClickDelete(e, params)
+          }}
+        >
+          Delete Session
+        </Button>
+      </strong>
+    )
+  }
 
-                        <Paper>
-                            <div class={classes.top70}></div>
-                            <Grid item xs={6} >
-                                <Typography
-                                    align="center"
-                                    variant="h6"
-                                    className={classes.bold400}
-                                    color="textSecondary"
-                                >
-                                    Search Records :
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={6}>
-                                <TextField
-                                    align="center"
-                                    required={true}
-                                    name="id"
-                                    label="Session ID"
-                                   
-                                />
+  const pendingColumns = [
+    {
+      field: 'id',
+      headerName: 'Unique-ID',
+      width: 170,
+      headerAlign: 'left',
+    },
+    {
+      field: "title",
+      headerName: 'Session title',
+      width: 180,
+      headerAlign: 'left',
+    },
 
-                            </Grid>
-                            <div class={classes.top70}></div>
-                        </Paper>
-                    </Grid>
-                    <Notification notify={notify} setNotify={setNotify} />
-                </Grid> */}
+    {
+      field: 'date',
+      headerName: 'Date',
+      width: 140,
+      headerAlign: 'center',
+    },
+    {
+      field: 'startTime',
+      headerName: 'Time',
+      width: 120,
+      headerAlign: 'center',
+    },
+    {
+      field: 'Edit',
+      headerName: 'Edit',
+      renderCell: renderPendingDetailsEditButton,
+      disableClickEventBubbling: true,
+      width: 120,
+      align: 'center',
+      headerAlign: 'center',
 
-<div style={{ height: 400, width: '100%' }}>
-      <DataGrid 
-        autoHeight
-        rows={data}
-        columns={columns}
-        pageSize={6}
-        onRowDoubleClick
-        disableSelectionOnClick
-      />
-    </div>
-            </Container>
-        </Layout>
-    );
+    },
+    {
+      field: 'delete',
+      headerName: 'Discard',
+      renderCell: renderDeleteButton,
+      disableClickEventBubbling: true,
+      width: 150,
+      align: 'center',
+      headerAlign: 'center',
+
+    },
+  ];
+
+  const columns = [
+    {
+      field: 'id',
+      headerName: 'Unique-ID',
+      width: 220,
+      headerAlign: 'left',
+    },
+    {
+      field: "title",
+      headerName: 'Session title',
+      width: 180,
+      headerAlign: 'left',
+    },
+    {
+      field: 'date',
+      headerName: 'Date',
+      width: 140,
+      headerAlign: 'center',
+    },
+    {
+      field: 'startTime',
+      headerName: 'Time',
+      width: 130,
+      headerAlign: 'center',
+    },
+    {
+      field: 'Edit',
+      headerName: 'View Details',
+      renderCell: renderDetailsButton,
+      disableClickEventBubbling: true,
+      width: 200,
+      align: 'center',
+      headerAlign: 'center',
+
+    },
+  ];
+
+  useEffect(() => {
+    async function getList() {
+      const idToken = await currentUser.getIdToken(true); //get the token of the current user
+      var toSend = {
+        token: idToken,
+      };
+      try {
+        fetch("/getTrainingRecords", {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify(toSend),
+        })
+          .then((res) => res.json()) //retrieving the request from backend
+          .then((data) => getAllData(data));
+        //printing it to the console
+      } catch (err) { }
+    }
+    getList(); //executing it
+  }, []);
+
+  var completedData = Alldata.filter(function (sessions) {
+    return sessions.completed == true;
+  });
+  var scheduledData = Alldata.filter(function (sessions) {
+    return sessions.completed != true;
+  });
+
+  console.log(Alldata);
+  return (
+    <Layout>
+      <Container size="sm">
+        <Typography style={{ marginTop: "50px", marginBottom: 50 }} size="12px" variant="h5" color="textSecondary">
+          <strong> Pending Training Sessions </strong>
+        </Typography>
+
+        <div style={{ height: 300, width: '100%' }}>
+          <DataGrid
+            autoHeight
+            rows={scheduledData}
+            columns={pendingColumns}
+            pageSize={3}
+            onRowDoubleClick
+            disableSelectionOnClick
+          />
+        </div>
+        <Typography style={{ marginTop: "50px", marginBottom: 50 }} size="12px" variant="h5" color="textSecondary">
+          <strong> Completed Training Sessions </strong>
+        </Typography>
+        <div style={{ height: 400, width: '100%' }}>
+          <DataGrid
+            autoHeight
+            rows={completedData}
+            columns={columns}
+            pageSize={8}
+            onRowDoubleClick
+            disableSelectionOnClick
+          />
+        </div>
+        <AlertDialogSlide
+          data={passingData}
+          participants={passingDataParticipants}
+          trainingTimes={passingDataTrainingTimes}
+          open={open}
+          setOpen={setOpen}
+        >
+        </AlertDialogSlide>
+        <AlertDialog
+          dataConfirm={passingDataDelete}
+          openConfirm={openConfirm}
+          setOpenConfirm={setOpenConfirm}>
+        </AlertDialog>
+      </Container>
+    </Layout>
+  );
 }
