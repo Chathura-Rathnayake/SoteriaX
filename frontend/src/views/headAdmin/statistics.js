a5794f205d22c824790bda0ac86bb2ca0415606a
Chathura <c.rathnayake97@gmail.com>
2021-09-21 04:50:22 +0530

    Implemented the drowning cases with time of the year plot

--- frontend/src/views/headAdmin/statistics.js
+++ frontend/src/views/headAdmin/statistics.js
@@ -12,11 +12,12 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TablePagination from "@material-ui/core/TablePagination";
 import TableRow from "@material-ui/core/TableRow";
-import { Button } from "@material-ui/core";
+import { Button, Grid } from "@material-ui/core";
 import { useAuth } from "../../contexts/AuthContext";
 import PollIcon from "@material-ui/icons/Poll";
 import PlayCircleFilledWhiteIcon from "@material-ui/icons/PlayCircleFilledWhite";
 import { storage } from "../../firebase";
+import { Line } from "react-chartjs-2";
 
 //dialog box
 import StatDialog from "../../components/headAdmin/statDialog";
@@ -26,7 +27,7 @@ const useStyles = makeStyles({
     width: "100%",
   },
   container: {
-    maxHeight: 440,
+    maxHeight: 250,
   },
 });
 
@@ -56,8 +57,10 @@ export default function Statistics() {
   const classes = useStyles();
   const [requests, setRequests] = useState([]);
   const [toDialogBox, setToDialogBox] = useState([]);
+  const [cases, setCases] = useState([]);
+  const [casesCount, setCasesCount] = useState([]);
   const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [rowsPerPage, setRowsPerPage] = React.useState(4);
 
   //session data
   const { currentUser } = useAuth();
@@ -82,7 +85,6 @@ export default function Statistics() {
   };
 
   const handleLinkClick = (data) => {
-
     // Create a reference to the file we want to download
     var storageRef = storage.ref();
     var videosRef = storageRef.child(
@@ -111,6 +113,7 @@ export default function Statistics() {
       .get()
       .then((querySnapshot) => {
         let tempMissionDataArray = [];
+        let tempCasesArray = [];
         querySnapshot.forEach((doc) => {
           // doc.data() is never undefined for query doc snapshots
           let tempMissionDataObject = {
@@ -120,122 +123,221 @@ export default function Statistics() {
             timeline: doc.data().timeline,
             endTime: doc.data().endTime,
           };
-          // const propertyValues = Object.values(tempMissionDataObject.timeline);
-          // console.log("timeline type", propertyValues[0]);
-          console.log(tempMissionDataObject.endTime.seconds);
+
+          tempCasesArray.push(tempMissionDataObject.date);
           tempMissionDataArray.push(tempMissionDataObject); //pushing each mission data to the array one by one
         });
         //set the request state - this is used to display the table
         setRequests(tempMissionDataArray);
+
+        //counting cases reported in each month to plot the graph
+        let monthsArray = [];
+        let arrayLength = tempCasesArray.length;
+        //putting month from all the date values read to a new array called monthsArray
+        for (let i = 0; i < arrayLength; i++) {
+          let dateParts = tempCasesArray[i].split("/");
+          monthsArray.push(dateParts[0]);
+        }
+
+        //counting distinct values in the months array
+        let counts = {};
+        for (let i = 0; i < monthsArray.length; i++) {
+          counts[monthsArray[i]] = 1 + (counts[monthsArray[i]] || 0);
+        }
+        console.log(counts);
+
+        let tempCasesCount = []; //this array will hold the cases reported in each month for all completed missions
+        for (let i = 1; i <= 12; i++) {
+          if (counts[i]) {
+            tempCasesCount.push(counts[i]);
+          } else {
+            tempCasesCount.push(0);
+          }
+        }
+        setCasesCount(tempCasesCount); //set the cases count
       })
       .catch((error) => {
         console.log("Error getting mission data: ", error);
       });
   }, []);
 
+  const caseData = {
+    labels: [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ],
+    datasets: [
+      {
+        label: "Drowning Cases",
+        fill: true,
+        lineTension: 0.5,
+        backgroundColor: "#FFB344",
+        borderColor: "#E05D5D",
+        borderWidth: 2,
+        data: casesCount,
+      },
+    ],
+  };
+
   return (
     <Layout>
-      <Container size="sm">
-        <div>
-          <Typography variant="h5" color="textSecondary">
-            Past Missions
-          </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Container size="sm">
+            <div>
+              <Typography variant="h6" color="textSecondary">
+                Past Missions
+              </Typography>
+              <br />
 
-          {/* the table */}
+              {/* the table */}
 
-          <Paper className={classes.root}>
-            <TableContainer className={classes.container}>
-              <Table stickyHeader aria-label="sticky table">
-                <TableHead>
-                  <TableRow>
-                    {columns.map((column) => (
-                      <TableCell
-                        key={column.id}
-                        style={{ minWidth: column.minWidth }}
-                      >
-                        {column.label}
-                      </TableCell>
-                    ))}
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {requests
-                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                    .map((request) => {
-                      return (
-                        <TableRow
-                          hover
-                          role="checkbox"
-                          tabIndex={-1}
-                          key={request.id}
-                        >
-                          {columns.map((column) => {
-                            const value = request[column.id];
-                            return (
-                              <TableCell key={column.id}>
-                                {column.format && typeof value === "number"
-                                  ? column.format(value)
-                                  : value}
+              <Paper className={classes.root}>
+                <TableContainer className={classes.container}>
+                  <Table stickyHeader size="small" aria-label="sticky table">
+                    <TableHead>
+                      <TableRow>
+                        {columns.map((column) => (
+                          <TableCell
+                            key={column.id}
+                            style={{ minWidth: column.minWidth }}
+                          >
+                            {column.label}
+                          </TableCell>
+                        ))}
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {requests
+                        .slice(
+                          page * rowsPerPage,
+                          page * rowsPerPage + rowsPerPage
+                        )
+                        .map((request) => {
+                          return (
+                            <TableRow
+                              hover
+                              role="checkbox"
+                              tabIndex={-1}
+                              key={request.id}
+                            >
+                              {columns.map((column) => {
+                                const value = request[column.id];
+                                return (
+                                  <TableCell key={column.id}>
+                                    {column.format && typeof value === "number"
+                                      ? column.format(value)
+                                      : value}
 
-                                {column.id === "view" && (
-                                  <Button
-                                    mini={true}
-                                    variant="fab"
-                                    zDepth={0}
-                                    onClick={() => handleLinkClick(request)}
-                                    // onClick={() => approveUserRequest(request)}
-                                  >
-                                    <PlayCircleFilledWhiteIcon
-                                      style={{ color: "#039BE5" }}
-                                    />
-                                  </Button>
-                                )}
+                                    {column.id === "view" && (
+                                      <Button
+                                        mini={true}
+                                        variant="fab"
+                                        zDepth={0}
+                                        onClick={() => handleLinkClick(request)}
+                                        // onClick={() => approveUserRequest(request)}
+                                      >
+                                        <PlayCircleFilledWhiteIcon
+                                          style={{ color: "#039BE5" }}
+                                        />
+                                      </Button>
+                                    )}
 
-                                {column.id === "stats" && (
-                                  <div>
-                                    <Button
-                                      mini={true}
-                                      variant="fab"
-                                      onClick={() =>
-                                        handleStatBoxClick(request)
-                                      }
-                                      // onClick={() =>
-                                      //   deleteUserRequest(request["id"])
-                                      // }
-                                    >
-                                      <PollIcon style={{ color: "orange" }} />
-                                    </Button>
+                                    {column.id === "stats" && (
+                                      <div>
+                                        <Button
+                                          mini={true}
+                                          variant="fab"
+                                          onClick={() =>
+                                            handleStatBoxClick(request)
+                                          }
+                                          // onClick={() =>
+                                          //   deleteUserRequest(request["id"])
+                                          // }
+                                        >
+                                          <PollIcon
+                                            style={{ color: "orange" }}
+                                          />
+                                        </Button>
 
-                                    {/* the mission data dialog box */}
+                                        {/* the mission data dialog box */}
 
-                                    <StatDialog
-                                      open={open}
-                                      setOpen={setOpen}
-                                      data={toDialogBox}
-                                    />
-                                  </div>
-                                )}
-                              </TableCell>
-                            );
-                          })}
-                        </TableRow>
-                      );
-                    })}
-                </TableBody>
-              </Table>
-            </TableContainer>
-            <TablePagination
-              rowsPerPageOptions={[5, 10, 25, 100]}
-              component="div"
-              count={requests.length}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
-            />
-          </Paper>
-        </div>
-      </Container>
+                                        <StatDialog
+                                          open={open}
+                                          setOpen={setOpen}
+                                          data={toDialogBox}
+                                        />
+                                      </div>
+                                    )}
+                                  </TableCell>
+                                );
+                              })}
+                            </TableRow>
+                          );
+                        })}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+                <TablePagination
+                  rowsPerPageOptions={[5, 10, 25, 100]}
+                  component="div"
+                  count={requests.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  onPageChange={handleChangePage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+              </Paper>
+            </div>
+          </Container>
+        </Grid>
+
+        <Grid item xs={8}>
+          <Container size="sm">
+            <Paper style={{ padding: "10px" }}>
+              <Typography variant="subtitle1" align="center" color="initial">
+                Number of drowning cases with the time of the year
+              </Typography>
+
+              <div>
+                <Line
+                  width={120}
+                  height={230}
+                  data={caseData}
+                  options={{
+                    maintainAspectRatio: false,
+                    responsive: true,
+                    title: {
+                      display: true,
+                      text: "",
+                      fontSize: 20,
+                    },
+                    legend: {
+                      display: false,
+                      position: "right",
+                    },
+                  }}
+                />
+              </div>
+            </Paper>
+          </Container>
+        </Grid>
+        <Grid item xs={4}>
+          <Container size="sm">
+            <Paper>test</Paper>
+          </Container>
+        </Grid>
+      </Grid>
     </Layout>
   );
 }
